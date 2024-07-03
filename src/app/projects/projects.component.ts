d5e2bdf3a4c3e020ee8777cbee2a3b9fd8dba980
minnukota381 <minnukota381@gmail.com>
2024-07-03 07:10:42 +0530

    feat: Set up initial structure for portfolio site

---
+++ src/app/projects/projects.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-projects',
+  standalone: true,
+  imports: [],
+  templateUrl: './projects.component.html',
+  styleUrl: './projects.component.scss'
+})
+export class ProjectsComponent {
+
+}
