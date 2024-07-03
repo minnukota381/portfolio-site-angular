6ea5ed31b32629b4d9afd27113b77635e693c5c3
minnukota381 <minnukota381@gmail.com>
2024-07-03 21:51:21 +0530

    Fixed routing import issue by adjusting app.config.ts to import routes correctly.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
 
-export const routes: Routes = [];
+// Define your routes as an array of Route objects
+export const routes: Routes = [
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
+  { path: 'home', component: HomeComponent },
+  // Add more routes as needed
+];
