52ff3ad6ff01d90e7313a5bd6204279aeeff60c6
minnukota381 <minnukota381@gmail.com>
2024-07-14 11:57:11 +0530

    feat: updated

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, AfterViewInit } from '@angular/core';
+import Typed from 'typed.js';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrl: './home.component.scss'
+  styleUrls: ['./home.component.scss']
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit, AfterViewInit {
 
+  ngOnInit() {
+    // Any other initialization logic
+  }
+
+  ngAfterViewInit() {
+    this.initTyped();
+  }
+
+  private initTyped() {
+    const options = {
+      strings: ['Programmer', 'Developer', 'Designer', 'Innovator'],
+      typeSpeed: 100,
+      backSpeed: 50,
+      loop: true
+    };
+
+    const typed = new Typed('#typed-text', options);
+  }
 }
