f7c57305b4762cb5d964862171e227c68b339bab
minnukota381 <minnukota381@gmail.com>
2024-07-26 08:24:55 +0530

    "Added imports for About, Projects, Skills, Resume, and Contact components and configured routes for them in app-routing.module.ts"

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,21 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { ProjectsComponent } from './projects/projects.component';
+import { SkillsComponent } from './skills/skills.component';
+import { ResumeComponent } from './resume/resume.component';
+import { ContactComponent } from './contact/contact.component';
 
 const routes: Routes = [
   { path: '', component: HomeComponent },
-  // Add other routes here as you create new components
+  { path: 'about', component: AboutComponent },
+  { path: 'projects', component: ProjectsComponent },
+  { path: 'skills', component: SkillsComponent },
+  { path: 'resume', component: ResumeComponent },
+  { path: 'contact', component: ContactComponent },
+  //wildcard route for 404 page
+  { path: '**', redirectTo: '', pathMatch: 'full' }
 ];
 
 @NgModule({
