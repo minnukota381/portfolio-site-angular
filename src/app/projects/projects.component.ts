02bab41f8c644316a218f765beddb3a6c3b4cb42
minnukota381 <minnukota381@gmail.com>
2024-07-28 20:59:52 +0530

    "Refactored ProjectsComponent: updated HTML, SCSS, and TS files to display projects in a grid layout with a new design"

--- src/app/projects/projects.component.ts
+++ src/app/projects/projects.component.ts
@@ -1,10 +1,59 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+interface Project {
+  title: string;
+  description: string;
+  image: string;
+  link: string;
+}
 
 @Component({
   selector: 'app-projects',
   templateUrl: './projects.component.html',
-  styleUrl: './projects.component.scss'
+  styleUrls: ['./projects.component.scss']
 })
-export class ProjectsComponent {
+export class ProjectsComponent implements OnInit {
+  projects: Project[] = [
+    {
+      title: 'Project 1',
+      description: 'Brief description of Project 1.',
+      image: 'assets/project1.jpg',
+      link: '#'
+    },
+    {
+      title: 'Project 2',
+      description: 'Brief description of Project 2.',
+      image: 'assets/project2.jpg',
+      link: '#'
+    },
+    {
+      title: 'Project 3',
+      description: 'Brief description of Project 3.',
+      image: 'assets/project3.jpg',
+      link: '#'
+    },
+    {
+      title: 'Project 4',
+      description: 'Brief description of Project 4.',
+      image: 'assets/project4.jpg',
+      link: '#'
+    },
+    {
+      title: 'Project 5',
+      description: 'Brief description of Project 5.',
+      image: 'assets/project5.jpg',
+      link: '#'
+    },
+    {
+      title: 'Project 6',
+      description: 'Brief description of Project 6.',
+      image: 'assets/project6.jpg',
+      link: '#'
+    }
+  ];
 
+  constructor() { }
+
+  ngOnInit(): void {
+  }
 }
