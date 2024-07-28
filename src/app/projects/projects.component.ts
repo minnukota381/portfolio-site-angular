a670466914d6ea868808fc4e601ffe48fdacf267
minnukota381 <minnukota381@gmail.com>
2024-07-28 21:05:54 +0530

    "Refactored ProjectsComponent: replaced grid layout with Bootstrap cards, removed project data from component, and updated styles"

--- src/app/projects/projects.component.ts
+++ src/app/projects/projects.component.ts
@@ -1,59 +1,10 @@
-import { Component, OnInit } from '@angular/core';
-
-interface Project {
-  title: string;
-  description: string;
-  image: string;
-  link: string;
-}
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-projects',
   templateUrl: './projects.component.html',
-  styleUrls: ['./projects.component.scss']
+  styleUrl: './projects.component.scss'
 })
-export class ProjectsComponent implements OnInit {
-  projects: Project[] = [
-    {
-      title: 'Project 1',
-      description: 'Brief description of Project 1.',
-      image: 'assets/project1.jpg',
-      link: '#'
-    },
-    {
-      title: 'Project 2',
-      description: 'Brief description of Project 2.',
-      image: 'assets/project2.jpg',
-      link: '#'
-    },
-    {
-      title: 'Project 3',
-      description: 'Brief description of Project 3.',
-      image: 'assets/project3.jpg',
-      link: '#'
-    },
-    {
-      title: 'Project 4',
-      description: 'Brief description of Project 4.',
-      image: 'assets/project4.jpg',
-      link: '#'
-    },
-    {
-      title: 'Project 5',
-      description: 'Brief description of Project 5.',
-      image: 'assets/project5.jpg',
-      link: '#'
-    },
-    {
-      title: 'Project 6',
-      description: 'Brief description of Project 6.',
-      image: 'assets/project6.jpg',
-      link: '#'
-    }
-  ];
+export class ProjectsComponent {
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
 }
