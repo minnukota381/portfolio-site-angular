11b18ccd3a2b4443571ae8b43886561ac5bb5817
minnukota381 <minnukota381@gmail.com>
2024-07-28 21:45:28 +0530

    "Refactored ProjectsComponent: updated HTML, SCSS, and TS files to display projects in a grid with overlay effects and tags"

--- src/app/projects/projects.component.ts
+++ src/app/projects/projects.component.ts
@@ -1,10 +1,60 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+interface Project {
+  title: string;
+  description: string;
+  image: string;
+  link: string;
+  tags: string[];
+}
 
 @Component({
   selector: 'app-projects',
   templateUrl: './projects.component.html',
-  styleUrl: './projects.component.scss'
+  styleUrls: ['./projects.component.scss']
 })
-export class ProjectsComponent {
+export class ProjectsComponent implements OnInit {
+  projects: Project[] = [
+    {
+      title: 'Project 1',
+      description: 'An innovative web application that revolutionizes task management.',
+      image: 'assets/project1.jpg',
+      link: '#',
+      tags: ['Angular', 'TypeScript', 'SCSS']
+    },
+    {
+      title: 'Project 1',
+      description: 'An innovative web application that revolutionizes task management.',
+      image: 'assets/project1.jpg',
+      link: '#',
+      tags: ['Angular', 'TypeScript', 'SCSS']
+    },
+    {
+      title: 'Project 1',
+      description: 'An innovative web application that revolutionizes task management.',
+      image: 'assets/project1.jpg',
+      link: '#',
+      tags: ['Angular', 'TypeScript', 'SCSS']
+    },
+    {
+      title: 'Project 1',
+      description: 'An innovative web application that revolutionizes task management.',
+      image: 'assets/project1.jpg',
+      link: '#',
+      tags: ['Angular', 'TypeScript', 'SCSS']
+    },
+    {
+      title: 'Project 1',
+      description: 'An innovative web application that revolutionizes task management.',
+      image: 'assets/project1.jpg',
+      link: '#',
+      tags: ['Angular', 'TypeScript', 'SCSS']
+    },
+    // Add more projects with similar structure
+  ];
 
+  constructor() { }
+
+  ngOnInit(): void {
+  }
 }
