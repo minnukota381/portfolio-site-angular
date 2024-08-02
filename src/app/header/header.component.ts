34a7825ff4fc73f427494d09c5121da1e70347e2
minnukota381 <minnukota381@gmail.com>
2024-08-03 00:09:28 +0530

    "Updated angular.json to include additional Bootstrap CSS and JS files, added HostListener and toggleNavbar function to HeaderComponent, and added Bootstrap scripts to index.html"

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrl: './header.component.scss'
+  styleUrls: ['./header.component.scss']
 })
 export class HeaderComponent {
+  isNavbarCollapsed = true;
 
+  @HostListener('window:scroll', ['$event'])
+  onWindowScroll() {
+    const scrollPos = window.scrollY;
+    const navbar = document.querySelector('.navbar');
+    if (scrollPos > 50) {
+      navbar?.classList.add('scrolled');
+    } else {
+      navbar?.classList.remove('scrolled');
+    }
+  }
+
+  toggleNavbar() {
+    this.isNavbarCollapsed = !this.isNavbarCollapsed;
+  }
 }
